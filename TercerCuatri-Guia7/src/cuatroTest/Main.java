20c1a215f410a6458974f3776918621f60a1cbea
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 21:43:22 -0300

    Clase Main ej4
    Enunciado

---
+++ TercerCuatri-Guia7/src/cuatroTest/Main.java
@@ -0,0 +1,12 @@
+package cuatroTest;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*Crea una clase base llamada Instrumento y dos subclases llamadas Guitarra y Bateria. Agrega un método
+        llamado tocar() a la clase Instrumento y redefine este método en las subclases para que los guitarristas
+        toquen acordes y los bateristas toquen ritmos. Luego, crea un conjunto de instrumentos que incluya tanto
+        guitarras como baterías y haz que cada instrumento en el conjunto sea tocado.*/
+
+    }
+}
