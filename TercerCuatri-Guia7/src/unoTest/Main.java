89778b62f51b44cc0b4394d766f82223cb207691
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 21:44:04 -0300

    Clase Main ej1
    Enunciado

---
+++ TercerCuatri-Guia7/src/unoTest/Main.java
@@ -0,0 +1,12 @@
+package unoTest;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*Crea una clase base llamada Figura y dos subclases llamadas Circulo y Rectangulo. Agrega un método llamado
+        area() a la clase Figura y redefine este método en las subclases para calcular el área del círculo y
+        el rectángulo respectivamente. Luego, crea un conjunto de figuras que incluya tanto círculos como rectángulos
+        y calcula la suma total de las áreas de todas las figuras en el conjunto.*/
+
+    }
+}
