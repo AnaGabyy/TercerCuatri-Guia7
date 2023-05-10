023908525fd9d2006c0a0935d0e1146a9597a738
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 21:43:07 -0300

    Clase Main ej5
    Enunciado

---
+++ TercerCuatri-Guia7/src/cincoTest/Main.java
@@ -0,0 +1,12 @@
+package cincoTest;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*Crea una clase base llamada Vehiculo y dos subclases llamadas Automovil y Motocicleta.
+        Agrega un método llamado acelerar() a la clase Vehiculo y redefine este método en las subclases
+        para que los automóviles aceleren y las motocicletas aceleren aún más rápido. Luego, crea un conjunto
+        de vehículos que incluya tanto automóviles como motocicletas y haz que cada vehículo en el conjunto acelere.*/
+
+    }
+}
