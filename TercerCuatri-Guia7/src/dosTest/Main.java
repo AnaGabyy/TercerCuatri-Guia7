fe62189d4203c5fb2fea2a6d0714a9e8a604b8b8
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 21:43:37 -0300

    Clase Main ej2
    Enunciado

---
+++ TercerCuatri-Guia7/src/dosTest/Main.java
@@ -0,0 +1,12 @@
+package dosTest;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*Crea una clase base llamada Animal y dos subclases llamadas Perro y Gato. Agrega un método
+        llamado sonido() a la clase Animal y redefine este método en las subclases para que los perros
+        ladren y los gatos maúllen. Luego, crea un conjunto de animales que incluya tanto perros como gatos
+        y haz que cada animal en el conjunto haga su sonido.*/
+
+    }
+}
