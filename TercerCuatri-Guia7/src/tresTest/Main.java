0346c24bf44f9ed2dd59322608d4949d4e1268fb
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 21:43:51 -0300

    Clase Main ej3
    Enunciado

---
+++ TercerCuatri-Guia7/src/tresTest/Main.java
@@ -0,0 +1,13 @@
+package tresTest;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*Crea una clase base llamada Persona y dos subclases llamadas Empleado y Cliente.
+        Agrega un método llamado nombre_completo() a la clase Persona y redefine este método en las
+        subclases para incluir información específica del empleado o cliente, como su cargo o su número
+        de cliente. Luego, crea un conjunto de personas que incluya tanto empleados como clientes y haz que
+        cada persona en el conjunto imprima su nombre completo.*/
+
+    }
+}
